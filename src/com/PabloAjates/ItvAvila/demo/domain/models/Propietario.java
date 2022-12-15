c1dcaa63086d5cb2c8129937b5c663fbcbc937fb
pabloajates <115726046+pabloajates@users.noreply.github.com>
2022-12-15 18:03:20 +0100

    entidades e interfaz

---
+++ src/com/PabloAjates/ItvAvila/demo/domain/models/Propietario.java
@@ -0,0 +1,68 @@
+package com.PabloAjates.ItvAvila.demo.domain.models;
+
+public class Propietario {
+
+    private String Dni;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String provincia;
+    private String poblacion;
+    private String telefono;
+
+    public String getDni() {
+        return Dni;
+    }
+
+    public void setDni(String dni) {
+        Dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ src/com/PabloAjates/ItvAvila/demo/domain/models/Vehiculo.java
@@ -0,0 +1,10 @@
+package com.PabloAjates.ItvAvila.demo.domain.models;
+
+public interface Vehiculo {
+
+    public String getMatricula();
+    public String getMarca();
+    public String getModelo();
+    public String getColor();
+    public String getAñoMatriculacion();
+}
